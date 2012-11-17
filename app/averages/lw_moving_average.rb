5f94fc51c528f186b4e42a3f2344fe137194f502
Mohamed Adam Chaieb <m.adam-chaieb@live.com>
2012-11-16 23:33:18 -0500

    added LWMA class

--- app/averages/lw_moving_average.rb
+++ app/averages/lw_moving_average.rb
@@ -1,3 +1,12 @@
 class LWMovingAverage
-	
+	def initialize(term)
+		@term = term
+	end	
+
+	def compute(prices)
+		sum = 0
+		prices.each_with_index{|price,index| sum += price*index}
+		return (2.0/(prices.count*(prices.count+1)))*sum
+	end
 end
+LWMovingAverage.new(5).compute([1,2,3])
