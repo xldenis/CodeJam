c3cb341ad30c69a71fb2362a7958f766ff6e15e9
Mohamed Adam Chaieb <m.adam-chaieb@live.com>
2012-11-17 04:16:09 -0500

    fixed structures of Crossover class, commented

--- app/crossover.rb
+++ app/crossover.rb
@@ -3,24 +3,29 @@ class Crossover
 		@current_difference = nil
 	end 
 
-	def update_difference(short_average,long_average)
+	#updates the difference and detects any crossovers, returns the corresponding instruction
+	def update(short_average,long_average)
 		if @current_difference == nil
 			@current_difference = short_average - long_average
 		else
 			new_difference = short_average - long_average
-			issue_instruction(new_difference)
+			instruction = instruction(new_difference)
 			@current_difference = new_difference
 		end
+		instruction #return
 	end
 
-	def issue_instruction(new_diff)
+	private
+	#computes the corresponding instruction to the new state of averages
+	def instruction(new_diff)
 		instruction = ''
 		#if short term average crosses long term average from bottom
 		if @current_difference < 0 & new_diff > 0
 			instruction = 'B'
+		# if short term average crosses long term average from top
 		elsif @current_difference > 0 & new_diff < 0
 			instruction = 'S'
 		end
-		instruction
+		instruction #return
 	end
 end
