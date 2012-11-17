2e8ee9271c21b3deb9569427fea0e7642ec4a61f
Xavier Denis <xldenis@gmail.com>
2012-11-16 22:38:29 -0500

    Impliment circular arrays as a fifo class

--- main.rb
+++ main.rb
@@ -1,5 +1,9 @@
 require 'socket'
 Dir["./app/*"].each {|file| require file}
+FAST = 5
+SLOW = 20
 m = MarketWatcher.new("localhost",8211,"|")
+slowvalues = 
 m.puts("H")
-p m.gets
+p a.insert(m.gets)
+p a
