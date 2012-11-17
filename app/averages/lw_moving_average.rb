3701f794d1922b9e8555164b81a3aafb91b16e24
Mohamed Adam Chaieb <m.adam-chaieb@live.com>
2012-11-17 00:03:08 -0500

    fixed bug in LWMA class

--- app/averages/lw_moving_average.rb
+++ app/averages/lw_moving_average.rb
@@ -5,8 +5,7 @@ class LWMovingAverage
 
 	def compute(prices)
 		sum = 0
-		prices.each_with_index{|price,index| sum += price*index}
+		prices.each_with_index{|price,index| sum += price*(index+1)}
 		return (2.0/(prices.count*(prices.count+1)))*sum
 	end
 end
-LWMovingAverage.new(5).compute([1,2,3])
